c5783a2231082a3af182c967515d6756fabb90f7
AdamChb <115777662+AdamChb@users.noreply.github.com>
2025-04-10 12:21:30 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 
-const publicPath = process.env.NODE_ENV === 'production' ? '/portfolio/' : '/'
 module.exports = {
- publicPath: publicPath,
+  publicPath: './'
 }
